8591f315eb07811c18b2f6018659e5f05419b71e
Anna Weisbrodt <aweisbro@gmail.com>
2016-08-29 11:15:25 -0600

    pizza express complete

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const generateId = require('./lib/generate-id');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded( {extended: true }));
+
+app.use(express.static('static'));
+
+app.set('port', process.env.PORT || 3000);
+
+app.locals.pizzas = {};
+app.locals.title = 'Pizza Express';
+
+app.set('view engine', 'jade');
+
+app.get('/', (request, response) => {
+  response.send(app.locals.title);
+});
+
+app.get('/pizzas/:id', (request, response) => {
+  var pizza = app.locals.pizzas[request.params.id];
+  response.render('pizza', { pizza: pizza});
+});
+
+app.get('/', (request, response) => {
+  response.render('index');
+});
+
+app.post('/pizzas', (request, response) => {
+  if (!request.body.pizza) { return response.sendStatus(400); }
+  var id = generateId();
+  app.locals.pizzas[id] = request.body.pizza;
+  response.redirect('/pizzas/' + id);
+});
+
+
+if (!module.parent) {
+  app.listen(app.get('port'), () => {
+    console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+  });
+}
+
+module.exports = app;
